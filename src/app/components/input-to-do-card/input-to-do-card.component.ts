db0aef5b8b32d96ede6754961da584260bd99443
sova-nn <nmo1@ya.ru>
2020-04-02 14:38:33 +0300

    Добавлен элемент новой карточки, добавление в картотеку

---
+++ src/app/components/input-to-do-card/input-to-do-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-to-do-card',
+  templateUrl: './input-to-do-card.component.html',
+  styleUrls: ['./input-to-do-card.component.css']
+})
+export class InputToDoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/directives/fade-out.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appFadeOut]'
+})
+export class FadeOutDirective {
+
+  constructor() { }
+
+}
